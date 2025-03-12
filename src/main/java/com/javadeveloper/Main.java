c26c788d621171b4bb90bd1df175b5a3e306f3bb
Jaga-Jaga773 <ropp475@gmail.com>
2025-03-12 22:24:35 +0200

    init commit

--- src/main/java/com/javadeveloper/Main.java
+++ src/main/java/com/javadeveloper/Main.java
@@ -4,34 +4,13 @@ import java.util.Scanner;
 
 class Main {
 
+    public  Main() {}
+
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+        Transport bmw = new Transport(200.5f,2000, "Black", new byte[] {0,0,0});
 
-        System.out.print("Введіть число: ");
-        int number = scanner.nextInt();
-
-
-        int result = factorial(number);
-
-
-        System.out.println("Факторіал числа " + number + " дорівнює: " + result);
-
-        scanner.close();
-    }
-
-
-    public static int factorial(int n) {
-        int result = 1;
-
-
-        for (int i = 1; i <= n; i++) {
-            result *= i;
-        }
-
-        return result;
-
-
+        Transport truck = new Transport(130f,5000,"White"  );
 
 
 
