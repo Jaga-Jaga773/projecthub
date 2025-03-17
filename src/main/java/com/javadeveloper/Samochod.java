7c0f9eba159dc4941c1a87dd226c50682d5bfbc9
Jaga-Jaga773 <ropp475@gmail.com>
2025-03-17 18:34:41 +0200

    init commit

--- src/main/java/com/javadeveloper/Samochod.java
+++ src/main/java/com/javadeveloper/Samochod.java
@@ -1,80 +1,156 @@
 package com.javadeveloper;
 
-import java.time.Year;
-
 public class Samochod {
 
-    private final String  marka;
-    private  final  String model;
-    private final  int rocznik;
-    private int przebieg;
+    private  final  String marka;
+    private  final String model;
+    private  final  int rocznik;
+
+    private  double przebiek;
     private double spalenie;
-    private int predkoscMaksymalna;
-private int predkoscAktualna;
-private double pojemnoscZbiornika;
-private double stanPaliwa;
-private String numerRejestracyjny;
+    private double predkoscMaksymalna;
+    private double predkoscAktualna;
+    private double pojemnoscZbiornika;
+    private double stanPaliwa;
+    private String numerRejestracyjny;
 
-public Samochod(String  marka, String model,int rocznik,int przebieg, double spalenie,int predkoscMaksymalna, int predkoscAktualna,double pojemnoscZbiornika,double stanPaliwa, String numerRejestracyjny ) {
-if(rocznik <= 0) {
-    rocznik = Year.now().getValue();
-}
-this.marka = marka;
-this.model = model;
-this.rocznik = rocznik;
-this.przebieg = przebieg;
-this.spalenie = spalenie;
-this.predkoscMaksymalna = predkoscMaksymalna;
-this.pojemnoscZbiornika = pojemnoscZbiornika;
-this.numerRejestracyjny = numerRejestracyjny;
-this.predkoscAktualna = 0;
-this.stanPaliwa = 0;
 
-}
+    public Samochod(String marka, String model, int rocznik,double przebiek,double spalenie,int predkoscMaksymalna,int predkoscAktualna,double pojemnoscZbiornika, double stanPaliwa, String numerRejestracyjny ) {
 
-    public int zwrocWiek() {
-        return Year.now().getValue() - rocznik;
+        this.marka = marka;
+        this.model = model;
+        this.rocznik = 2025;
+        this.przebiek = 0;
+        this.spalenie = spalenie;
+        this.predkoscMaksymalna = predkoscMaksymalna;
+        this.predkoscAktualna = 0;
+        this.pojemnoscZbiornika = pojemnoscZbiornika;
+        this.stanPaliwa = 0;
+        this.numerRejestracyjny = numerRejestracyjny;
+
+
     }
 
-    public void zmienNrRej(String nowyNumer) {
-        this.numerRejestracyjny = nowyNumer;
+    public  String getMarka() {
+        return  marka;
     }
 
-    public void przyspiesz(int wartosc) {
-        this.predkoscAktualna = Math.min(predkoscAktualna + wartosc, predkoscMaksymalna);
+    public  String getModel() {
+        return model;
     }
 
-    public void zwolnij(int wartosc) {
-        this.predkoscAktualna = Math.max(predkoscAktualna - wartosc, 0);
+    public  int zwrocWiek(){
+        return rocznik;
     }
 
-    public void zatankuj(double ilosc) {
-        this.stanPaliwa = Math.min(stanPaliwa + ilosc, pojemnoscZbiornika);
+    public double getPrzebiek() {
+        return  przebiek;
     }
 
-    public void zatankujDoPelna() {
-        this.stanPaliwa = pojemnoscZbiornika;
+    public double getSpalenie() {
+        return spalenie;
     }
 
-    public boolean czyPusty() {
-        return stanPaliwa <= 0;
+    public double  getpredkoscMaksymalna () {
+        return  predkoscMaksymalna;
     }
 
-    public boolean czyPelny() {
-        return stanPaliwa >= pojemnoscZbiornika;
+    public double getPredkoscAktualna() {
+        return  predkoscAktualna;
     }
 
+    public  double getPojemnoscZbiornika () {
+        return pojemnoscZbiornika;
+    }
+
+    public double getStanPaliwa () {
+        return  stanPaliwa;
+    }
+
+    public  String  getNumerRejestracyjny () {
+        return  numerRejestracyjny;
+    }
+
+    public void  zmienNrRej (String newNumer) {
+        this.numerRejestracyjny = newNumer;
+    }
+
+    public  void  przypiesz( double wartosc ) {
+        predkoscAktualna += wartosc;
+        if(predkoscAktualna > predkoscMaksymalna){
+            predkoscAktualna = predkoscMaksymalna;
+        }
+    }
+
+    public  void  zwolnij( double wartosc) {
+        predkoscAktualna -= wartosc;
+        if(predkoscAktualna < 0){
+            predkoscAktualna = 0;
+        }
+    }
+
+    public void  ztankuj (double wartosc) {
+        stanPaliwa += wartosc;
+        if(stanPaliwa > 0){
+            stanPaliwa = pojemnoscZbiornika;
+        }
+    }
+
+    public  void  zatankujDoPelna () {
+        stanPaliwa = pojemnoscZbiornika;
+    }
+
+    public  boolean  czyPusty () {
+        if(stanPaliwa > 0) {
+            return  true;
+        } else {
+            return  false;
+        }
+
+    }
+
+    public  boolean czyPelny () {
+        if (stanPaliwa < 0) {
+            return  true;
+        }else {
+            return  false;
+        }
+    }
+
+    public  void wyswietl () {
+
+        System.out.println("Marka: " + marka);
+        System.out.println("Model: " + model);
+        System.out.println("Rocznik: " + rocznik);
+        System.out.println("Przebieg: " + przebiek);
+        System.out.println("Spalenie: " + spalenie + "1/100km");
+        System.out.println("Predkosc maksymalna: "  + predkoscMaksymalna + "km/h");
+        System.out.println("Predkosc aktualna: " + predkoscAktualna + "km/h");
+        System.out.println("Pojemnosc zbiornika: " + pojemnoscZbiornika);
+        System.out.println("Stan paliwa: " + stanPaliwa);
+        System.out.println("Numer rejestracyjny: " + numerRejestracyjny);
+
+    }
+
+    public  void zatrzymajsie() {
+        predkoscAktualna = 0;
+    }
+
+    public void wykonajTuning (double zwiekszeniePredkosci) {
+        predkoscMaksymalna += zwiekszeniePredkosci;
+    }
+
+    public void  jedz (double dystans) {
+        double potrzebnePaliwo = (spalenie / 100) * dystans;
+        if (stanPaliwa >= potrzebnePaliwo) {
+            przebiek += dystans;
+            stanPaliwa -= potrzebnePaliwo;
+        } else {
+            System.out.println("Brak wystarczajacej ilosci paliwa!");
+        }
 
 
-
-
-
-
-
-
-
-
-
+    }
 
 
 }
