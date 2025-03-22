fba9d3ca8cb6cdc93fb87345c3e9f61994f9f09e
Jaga-Jaga773 <ropp475@gmail.com>
2025-03-22 18:58:55 +0200

    init commit

--- src/main/java/com/javadeveloper/Main.java
+++ src/main/java/com/javadeveloper/Main.java
@@ -7,20 +7,64 @@ package com.javadeveloper;
 
     public static void main(String[] args) {
 
-Samochod samochod = new Samochod("Toyota", "Corolla", 2015, 700, 4, 180, 140, 30, 5, "TYU4673");
-samochod.wyswietl();
+        //Zadanie1
+//Scanner scan = new Scanner(System.in);
+//double[] tablica = new double [10];
+//double suma = 0;
+//
+//        System.out.println("Podaj 10 liczb: ");
+//        for (int i = 0;  i < tablica.length; i++){
+//            System.out.print( (i + 1) + " liczba: " );
+//            tablica[i] = scan.nextInt();
+//            suma += tablica[i];
+//        }
+//        System.out.println("Sriednia arytmetyczna wynosi: " + (suma/10));
+//
+//
+//scan.close();
 
-samochod.wykonajTuning(50);
-samochod.wyswietl();
+//Zadanie 2
+//        Scanner sc = new Scanner(System.in);
+//        int [] tablica = new int[8];
+//        System.out.println("Podaj 8 elementow z zakresu(-50-50): ");
+//
+//        for(int i = 0; i < tablica.length; i++) {
+//            System.out.print((i + 1) + " element: ");
+//            tablica[i] = sc.nextInt();
+//
+//            if (tablica[i] < -50 || tablica[i] > 50) {
+//                System.out.println("Nie podales liczby z zakresu(-50-50)!");
+//                return;
+//
+//            }
+//        }
+//
+//        int max = tablica[0];
+//        int min = tablica[0];
+//        int maxIndex = 0;
+//        int minIndex = 0;
+//
+//for(int i = 1; i < tablica.length; i++){
+//    if(tablica[i] > max){
+//        max = tablica[i];
+//                maxIndex = i;
+//    }
+//
+//    if(tablica[i] < min){
+//        min = tablica[i];
+//        minIndex = i;
+//    }
+//
+//}
+//
+//        System.out.println("Najwiekszy element to: " + max + " Indeks elementa: " + maxIndex);
+//        System.out.println("Najmniejszy element to: " + min + "Indeks elementa: " + minIndex);
+//
+//
 
-samochod.zmienNrRej("rt5789");
-samochod.wyswietl();
 
-samochod.jedz(120);
-samochod.wyswietl();
 
-samochod.przypiesz(4);
-samochod.wyswietl();
+
 
 
 
